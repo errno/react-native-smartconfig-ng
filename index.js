ea7e61a61ecb5542a19ba2723c16a3145e4b6b3f
Artem Godlevskyi <artem.hodlevskyy@gmail.com>
2019-06-23 18:33:47 +0300

    Second initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNSmartconfigNg } = NativeModules;
+
+export default RNSmartconfigNg;
---
+++ ios/RNSmartconfigNg.m
@@ -0,0 +1,13 @@
+
+#import "RNSmartconfigNg.h"
+
+@implementation RNSmartconfigNg
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
